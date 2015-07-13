0263659d760ce9bf758906e4d15071387bb4d7df
wonsama <imtough99@gmail.com>
2015-07-13 11:38:39 +0900

    단어 읽기 기능 좀 더 강화 중...

--- src/main/java/kr/co/jwsnt/core/JwMain.java
+++ src/main/java/kr/co/jwsnt/core/JwMain.java
@@ -1,18 +1,31 @@
 package kr.co.jwsnt.core;
 
+import java.io.IOException;
+
+import kr.co.jwsnt.core.parser.JwWordReaderUtil;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class JwMain {
 
+	private Logger logger = LoggerFactory.getLogger(JwMain.class);
+	
 	public static void main(String[] args)
 	{
 		new JwMain();
 	}
 	
-	Logger logger = LoggerFactory.getLogger(JwMain.class);
-	
 	public JwMain() {
-		logger.info("info");
+		
+		String PATH = "/Users/parkwon/Desktop/mans/";
+		String pathname = PATH + "정우snt_방카슈랑스_김종혁_중급.doc";
+		String[] words = {"개발"};
+		
+		try {
+			JwWordReaderUtil.isIncludeWords(pathname, words);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
